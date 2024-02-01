7f2d75dd51a6261b87b1a5d58e2f67af68b58eed
Dilpreetsingh1536 <151109865+Dilpreetsingh1536@users.noreply.github.com>
2024-02-01 00:19:14 -0500

    Changes

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+
+const app = express();
+
+app.set("view engine", "ejs");
+
+app.use(express.static("public"));
+
+app.listen(3000, () => {
+  console.log("Server is listening at port 3000");
+});
+
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+
+
+
