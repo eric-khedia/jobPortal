f2cb3f5438d2b7ed2efc0b1a40af7ae4ba3e7b3e
Dilpreetsingh1536 <151109865+Dilpreetsingh1536@users.noreply.github.com>
2024-02-01 00:40:30 -0500

    Changes

--- app.js
+++ app.js
@@ -14,5 +14,13 @@ app.get("/login", (req, res) => {
   res.render("login");
 });
 
+app.get("/signup", (req, res) => {
+    res.render("signup");
+  });
+
+app.get("/home", (req, res) => {
+    res.render("home");
+  });
+
 
 
